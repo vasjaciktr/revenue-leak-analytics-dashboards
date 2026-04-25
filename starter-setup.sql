74e9b08b061fabad9c0066261567eaf82291325e
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-04-25 18:16:50 +0300

    Update starter-setup.sql

--- starter-setup.sql
+++ starter-setup.sql
@@ -404,6 +404,48 @@ SELECT
 FROM transitions_raw;
 
 
+-- X. Cart sessions (with value)
+
+CREATE OR REPLACE TABLE `YOUR_PROJECT.leakonic.cart_sessions` AS
+
+WITH base AS (
+  SELECT
+    CONCAT(
+      user_pseudo_id,
+      '-',
+      CAST((SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS STRING)
+    ) AS session_id,
+
+    event_name,
+
+    ecommerce.value AS event_value
+
+  FROM `YOUR_PROJECT.YOUR_GA4_DATASET.events_*`
+  WHERE _TABLE_SUFFIX BETWEEN start_date AND end_date
+),
+
+aggregated AS (
+  SELECT
+    session_id,
+
+    MAX(IF(event_name = 'add_to_cart', 1, 0)) AS add_to_cart,
+    MAX(IF(event_name = 'view_cart', 1, 0)) AS view_cart,
+    MAX(IF(event_name = 'begin_checkout', 1, 0)) AS begin_checkout,
+    MAX(IF(event_name = 'add_payment_info', 1, 0)) AS add_payment_info,
+    MAX(IF(event_name = 'purchase', 1, 0)) AS purchase,
+
+    MAX(IF(event_name = 'add_to_cart', event_value, NULL)) AS cart_value
+
+  FROM base
+  WHERE session_id IS NOT NULL
+  GROUP BY session_id
+)
+
+SELECT *
+FROM aggregated
+WHERE add_to_cart = 1;
+
+
 -- 5. Validation checks
 
 CREATE OR REPLACE TABLE `YOUR_PROJECT.leakonic.validation_checks` AS
